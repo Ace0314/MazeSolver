edb7c11cb85b9dabee1021cad442d3da329e5978
Ace0314 <vampirez66@yahoo.com>
2024-06-02 16:14:05 -0500

    update

--- graphics.py
+++ graphics.py
@@ -5,7 +5,7 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -33,7 +33,7 @@ class Point:
         self.y = y
 
 
-def Line:
+class Line:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
